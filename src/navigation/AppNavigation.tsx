fc13348799f4bdb523bd9a27cf02e663ee319c3e
eeee0717 <chentao020717Work@outlook.com>
2025-05-25 16:03:34 +0800

    chore: add external url

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -23,7 +23,7 @@ export const AppNavigation: React.FC = () => {
   const welcomeShown = useAppSelector(state => state.app.welcomeShown)
 
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="AboutSettings">
       {!welcomeShown && <Stack.Screen options={{ headerShown: false }} name="Welcome" component={WelcomePage} />}
       <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
       <Stack.Screen options={{ headerShown: false }} name="Settings" component={SettingsPage} />
