9f5c8db31bc546a6d2c0ce6ce2c91d897c819526
eeee0717 <chentao020717Work@outlook.com>
2025-05-28 10:44:00 +0800

    chore

--- src/app/(routes)/index.tsx
+++ src/app/(routes)/index.tsx
@@ -2,7 +2,7 @@ import { Link } from '@react-navigation/native'
 import React from 'react'
 import { Keyboard, KeyboardAvoidingView, Platform } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Stack, Text, useTheme, View, YStack } from 'tamagui'
+import { Stack, styled, Text, useTheme, View, YStack } from 'tamagui'
 
 import { MessageInput } from '@/components/message-input'
 import { TopEntry } from '@/components/top-entry'
@@ -14,31 +14,37 @@ const HomeScreen = () => {
     <SafeAreaView style={{ flex: 1, backgroundColor: theme.background.val }}>
       <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }}>
         <YStack backgroundColor="$background" flex={1} onPress={Keyboard.dismiss}>
-          {/* 顶部导航组件 */}
           <TopEntry />
 
-          {/* 主要内容区域 */}
-          <YStack flex={1} justifyContent="center" alignItems="center">
+          <ContentContainer>
             <Link screen="Settings" params={{}}>
               <Stack>
                 <Text color="$color12">Go to Settings</Text>
               </Stack>
             </Link>
-          </YStack>
+          </ContentContainer>
 
-          {/* 底部输入框 */}
-          <View
-            marginHorizontal="$3"
-            paddingHorizontal={16}
-            paddingVertical={12}
-            backgroundColor="$gray2"
-            borderRadius={9}>
+          <InputContainer>
             <MessageInput />
-          </View>
+          </InputContainer>
         </YStack>
       </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
+
+const ContentContainer = styled(YStack, {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center'
+})
+
+const InputContainer = styled(View, {
+  marginHorizontal: '$3',
+  paddingHorizontal: 16,
+  paddingVertical: 12,
+  backgroundColor: '$gray2',
+  borderRadius: 9
+})
 
 export default HomeScreen
