9b195a1c106c72d9b6abd4627df9675d7ab957a4
Pleasurecruise <3196812536@qq.com>
2025-06-02 21:53:14 +0800

    feat: update initial route to Home and enhance Japanese, Russian, and Chinese translations

--- src/app/(routes)/_layout.tsx
+++ src/app/(routes)/_layout.tsx
@@ -30,7 +30,7 @@ const AppStackLayout: FC = () => {
   const welcomeShown = useAppSelector(state => state.app.welcomeShown)
 
   return (
-    <Stack.Navigator initialRouteName="AgentPage">
+    <Stack.Navigator initialRouteName="Home">
       {!welcomeShown && <Stack.Screen options={{ headerShown: false }} name="Welcome" component={WelcomePage} />}
       <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
       <Stack.Screen options={{ headerShown: false }} name="Settings" component={SettingsPage} />
