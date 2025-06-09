3141bc9b4553ede2b4a0b488309d8e34fd68329c
eeee0717 <chentao020717Work@outlook.com>
2025-06-09 09:40:38 +0800

    chore: remove db and fix style

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,6 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+// import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
-export const test = sqliteTable('test', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  name: text('name').notNull()
-})
+// export const test = sqliteTable('test', {
+//   id: integer('id').primaryKey({ autoIncrement: true }),
+//   name: text('name').notNull()
+// })
