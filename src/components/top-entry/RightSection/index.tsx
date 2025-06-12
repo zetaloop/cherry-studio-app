7538547757750b0cfde048646271715123268b49
eeee0717 <chentao020717Work@outlook.com>
2025-06-12 23:00:05 +0800

    fix some errors

--- src/components/top-entry/RightSection/index.tsx
+++ src/components/top-entry/RightSection/index.tsx
@@ -3,15 +3,12 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Button, XStack } from 'tamagui'
 
-import { useRightSectionController } from '../hooks/useRightSectionController'
-
 export const RightSection: React.FC = () => {
   const { t } = useTranslation()
-  const { createNewTopicHandler } = useRightSectionController()
 
   return (
     <XStack alignItems="center" justifyContent="flex-end">
-      <Button size={24} circular chromeless icon={<SquarePen size={24} />} onPress={createNewTopicHandler} />
+      <Button size={24} circular chromeless icon={<SquarePen size={24} />} />
     </XStack>
   )
 }
