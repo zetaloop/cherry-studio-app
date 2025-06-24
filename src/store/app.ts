dddde9062b1887a6be6a3cf0f36875136d1c3ceb
eeee0717 <chentao020717Work@outlook.com>
2025-06-24 21:04:44 +0800

    fix

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,20 +1,24 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface RuntimeState {
+  initialized: boolean
   welcomeShown: boolean
 }
 
-const initialState: RuntimeState = { welcomeShown: false }
+const initialState: RuntimeState = { welcomeShown: false, initialized: false }
 
 const runtimeSlice = createSlice({
   name: 'runtime',
   initialState,
   reducers: {
+    setInitialized(state, action: PayloadAction<boolean>) {
+      state.initialized = action.payload
+    },
     setWelcomeShown(state, action: PayloadAction<boolean>) {
       state.welcomeShown = action.payload
     }
   }
 })
 
-export const { setWelcomeShown } = runtimeSlice.actions
+export const { setInitialized, setWelcomeShown } = runtimeSlice.actions
 export default runtimeSlice.reducer
