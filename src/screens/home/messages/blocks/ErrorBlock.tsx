e3b475c5b191b18753a04ca43889b3bb08e4d1a3
eeee0717 <chentao020717Work@outlook.com>
2025-07-15 15:30:48 +0800

    fix: error block style

--- src/screens/home/messages/blocks/ErrorBlock.tsx
+++ src/screens/home/messages/blocks/ErrorBlock.tsx
@@ -1,7 +1,7 @@
 import { AlertCircle } from '@tamagui/lucide-icons'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Card, Text, YStack } from 'tamagui'
+import { Card, Text, XStack, YStack } from 'tamagui'
 
 import { ErrorMessageBlock } from '@/types/message'
 
@@ -14,11 +14,11 @@ const MessageErrorInfo: React.FC<{ block: ErrorMessageBlock }> = ({ block }) => 
 
   const HTTP_ERROR_CODES = [400, 401, 403, 404, 429, 500, 502, 503, 504]
 
-  let errorMessage = ''
+  let errorMessage = t('errors.error_title')
   let description = ''
 
   if (block.error && HTTP_ERROR_CODES.includes(block.error?.status)) {
-    errorMessage = block.error?.message || ''
+    errorMessage = block.error?.message || t('errors.error_title')
     description = t(`error.http.${block.error.status}`)
   } else if (block?.error?.message) {
     const errorKey = `error.${block.error.message}`
@@ -28,17 +28,17 @@ const MessageErrorInfo: React.FC<{ block: ErrorMessageBlock }> = ({ block }) => 
   }
 
   return (
-    <Card backgroundColor="$red1" borderColor="$red6" borderWidth={1} margin="$2" padding="$3">
-      <YStack>
-        <YStack alignItems="flex-start" flexDirection="row">
-          <AlertCircle size={16} color="$red9" />
+    <Card padding={10} backgroundColor="$red20" borderColor="$red10" borderWidth={0.5}>
+      <YStack gap={10}>
+        <XStack gap={10}>
+          <AlertCircle size={16} color="$red100" />
           {errorMessage && (
-            <Text fontSize="$3" fontWeight="600" color="$red11">
+            <Text fontWeight="600" color="$red100">
               {errorMessage}
             </Text>
           )}
-        </YStack>
-        <Text fontSize="$2" color="$red11" lineHeight="$1">
+        </XStack>
+        <Text color="$red100" lineHeight="$1">
           {description}
         </Text>
       </YStack>
