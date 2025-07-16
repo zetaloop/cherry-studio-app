2fbb0e3ce9bd0be506cd97002d220966117af42a
eeee0717 <chentao020717Work@outlook.com>
2025-07-16 12:43:45 +0800

    fix: revert db

--- db/schema/assistants.ts
+++ db/schema/assistants.ts
@@ -1,7 +1,5 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
-import { websearch_providers } from './websearchProviders'
-
 export const assistants = sqliteTable('assistants', {
   id: text('id').notNull().unique().primaryKey(),
   name: text('name').notNull(),
@@ -16,11 +14,11 @@ export const assistants = sqliteTable('assistants', {
   default_model: text('default_model'),
   settings: text('settings'),
   enable_web_search: integer('enable_web_search', { mode: 'boolean' }),
-  websearch_provider_id: text('websearch_provider_id').references(() => websearch_providers.id),
   enable_generate_image: integer('enable_generate_image', { mode: 'boolean' }),
   // mcpServers: blob('mcp_servers'),
   knowledge_recognition: text('knowledge_recognition'),
   tags: text('tags'),
   group: text('group'),
   isStar: integer('isStar', { mode: 'boolean' })
+  // websearch_provider_id: text('websearch_provider_id')
 })
